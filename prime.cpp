90447bf4450d0db57c3a633bf972d800a2eaf6b0
Lokesh Punwani <113304906+Lokeshpunwani29@users.noreply.github.com>
2025-05-21 00:29:08 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -21,3 +21,28 @@ else cout<<"Number is not Prime."<<endl;
 
 return 0;
 }
+
+/*#include<iostream>
+using namespace std;
+
+bool checkPrime(int num)
+{
+    if(num<2) return false;
+    if(num==2) return true;
+    if(num%2==0) return false;
+    for(int i=3; i*i<num; i+=2)
+    {
+        if(num%i==0) return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int num;
+    cin>>num;
+    bool result = checkPrime(num);
+    if(result) cout<<"Prime";
+    else cout<<"Not Prime";
+    return 0;
+}*/
